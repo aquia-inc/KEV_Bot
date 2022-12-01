4acd82dc74200552b9c250e2660e0e6613aaa44d
Dustin Whited <dgwhited@gmail.com>
2022-12-01 09:41:25 -0600

    adds terraform deploy (#4)
    * sensitive vars and fmt
    
    * add deploy

--- lambda-iam.tf
+++ lambda-iam.tf
@@ -1,7 +1,7 @@
 data "aws_caller_identity" "current" {}
 locals {
-  account_id    = data.aws_caller_identity.current.account_id
-  region        = "us-east-1"
+  account_id = data.aws_caller_identity.current.account_id
+  region     = "us-east-1"
 }
 
 
--- variables.tf
+++ variables.tf
@@ -1,15 +1,19 @@
 variable "api_key" {
-  type = string
+  type      = string
+  sensitive = true
 }
 
 variable "api_key_secret" {
-  type = string
+  type      = string
+  sensitive = true
 }
 
 variable "access_token" {
-  type = string
+  type      = string
+  sensitive = true
 }
 
 variable "access_token_secret" {
-  type = string
+  type      = string
+  sensitive = true
 }
