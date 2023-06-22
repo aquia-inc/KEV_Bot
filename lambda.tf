3df1391e4a78e2f02fe6f4f001f25bbeb2451d4f
Dustin Whited <dgwhited@gmail.com>
2023-06-22 11:25:26 -0500

    pacify a linter

--- lambda.tf
+++ lambda.tf
@@ -38,14 +38,14 @@ resource "aws_lambda_layer_version" "requests_layer" {
   filename            = data.archive_file.requests_zip.output_path
   layer_name          = "Requests-KEV-IAC"
   compatible_runtimes = ["python3.9"]
-  source_code_hash    = filebase64sha256("${data.archive_file.requests_zip.output_path}")
+  source_code_hash    = filebase64sha256(data.archive_file.requests_zip.output_path)
 }
 
 resource "aws_lambda_layer_version" "tweepy_layer" {
   filename            = data.archive_file.tweepy_zip.output_path
   layer_name          = "tweepy-KEV-IAC"
   compatible_runtimes = ["python3.9"]
-  source_code_hash    = filebase64sha256("${data.archive_file.tweepy_zip.output_path}")
+  source_code_hash    = filebase64sha256(data.archive_file.tweepy_zip.output_path)
 }
 
 data "archive_file" "kev_lambda_zip" {
@@ -62,5 +62,5 @@ resource "aws_lambda_function" "kev_lambda" {
   timeout          = 240
   layers           = [aws_lambda_layer_version.requests_layer.arn, aws_lambda_layer_version.tweepy_layer.arn]
   runtime          = "python3.9"
-  source_code_hash = filebase64sha256("${data.archive_file.kev_lambda_zip.output_path}")
+  source_code_hash = filebase64sha256(data.archive_file.kev_lambda_zip.output_path)
 }
