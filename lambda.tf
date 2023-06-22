5692c3979b3f3b02dd93e4b86bb1f1128e2f2902
Dustin Whited <dgwhited@gmail.com>
2023-06-22 16:22:54 -0500

    update to python 3.10

--- lambda.tf
+++ lambda.tf
@@ -67,6 +67,6 @@ resource "aws_lambda_function" "kev_lambda" {
   handler          = "kev_lambda.lambda_handler"
   timeout          = 240
   layers           = [aws_lambda_layer_version.requests_layer.arn, aws_lambda_layer_version.tweepy_layer.arn]
-  runtime          = "python3.9"
+  runtime          = "python3.10"
   source_code_hash = data.archive_file.kev_lambda_zip.output_base64sha256
 }
