a1f362fc2d926300b92ece7ca6598d44e7d1d374
Dustin Whited <dgwhited@gmail.com>
2024-01-02 09:35:15 -0600

    remove twitter functions

--- kev_lambda.py
+++ kev_lambda.py
@@ -1,6 +1,5 @@
 import boto3
 import requests
-import tweepy
 
 
 def seed_the_table():
@@ -44,24 +43,6 @@ def save_new_cves(cve_list):
     return status_code
 
 
-def tweet(n_t):
-    api_key = get_encrypted("twitter_api_key_iac")
-    api_secret = get_encrypted("twitter_api_secret_iac")
-    access_token = get_encrypted("twitter_access_token_iac")
-    access_secret_token = get_encrypted("twitter_access_secret_token_iac")
-
-    # Authenticate
-    auth = tweepy.OAuth1UserHandler(
-        api_key, api_secret, access_token, access_secret_token
-    )
-    # create API Object
-    api = tweepy.API(auth)
-
-    # Create a tweet
-    for message in n_t:
-        api.update_status(message)
-
-
 def get_encrypted(key_name):
     client = boto3.client("ssm")
     response = client.get_parameter(Name=key_name, WithDecryption=True)
@@ -74,30 +55,29 @@ def lambda_handler(event, context):
     api_end = "https://www.cisa.gov/sites/default/files/feeds/known_exploited_vulnerabilities.json"
     kev_dict = requests.get(api_end).json()
     vul_dict = kev_dict["vulnerabilities"]
-    tweets = []
+    messages = []
     for key in vul_dict:
         if key["cveID"] not in old_cve_list:
-            new_tweet = f"{key['cveID']} - {key['vulnerabilityName']} has been added to the KEV catalog.  https://nvd.nist.gov/vuln/detail/{key['cveID']}"
+            new_message = f"{key['cveID']} - {key['vulnerabilityName']} has been added to the KEV catalog.  https://nvd.nist.gov/vuln/detail/{key['cveID']}"
             old_cve_list.append(key["cveID"])
-            tweets.append(new_tweet)
-    if len(tweets) > 0 and len(tweets) < 100:
-        # tweet(tweets)
+            messages.append(new_message)
+    if len(messages) > 0 and len(messages) < 100:
         save_new_cves(old_cve_list)
-        post_to_slack(tweets)
+        post_to_slack(messages)
 
-    return tweets
+    return messages
 
 
-def post_to_slack(tweets):
+def post_to_slack(messages):
     """Posts to slack
 
     Args:
-        tweets ([list]): Tweets to post to slack
+        messages ([list]): messages to post to slack
     """
 
     url = get_encrypted("slack_webhook_url")
 
-    for tweet in tweets:
+    for message in messages:
         payload = {
             "text": "Alert!! ",
             "attachments": [
@@ -105,7 +85,7 @@ def post_to_slack(tweets):
                     "blocks": [
                         {
                             "type": "section",
-                            "text": {"type": "mrkdwn", "text": tweet},
+                            "text": {"type": "mrkdwn", "text": message},
                         }
                     ]
                 }
